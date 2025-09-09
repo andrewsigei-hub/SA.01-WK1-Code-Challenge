276e2a6038c3eb556a9867cf283eb6f84ca38292
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-09-10 01:06:39 +0300

    Final commit 2

--- challenge3.js
+++ challenge3.js
@@ -1,19 +1,17 @@
 /* Calculate PAYE tax based on taxable income */
 function calculatePayee(taxableIncome) {
-    let tax = 0;
-    if (taxableIncome <= 24000) {
-        tax = taxableIncome * 0.1;
-    } else if (taxableIncome <= 32333) {
-        tax = (24000 * 0.1) + (taxableIncome - 24000) * 0.25;
-    } else {
-        tax = (24000 * 0.1) + (8333 * 0.25) + (taxableIncome - 32333) * 0.3;
-    }
-    /*  personal relief of KES 2,400 */
-    return(tax - 2400);
+  let tax = 0;
+  if (taxableIncome <= 24000) {
+    tax = taxableIncome * 0.1;
+  } else if (taxableIncome <= 32333) {
+    tax = 24000 * 0.1 + (taxableIncome - 24000) * 0.25;
+  } else {
+    tax = 24000 * 0.1 + 8333 * 0.25 + (taxableIncome - 32333) * 0.3;
+  }
+  /*  personal relief of KES 2,400 */
+  return tax - 2400;
 }
 
-function netSalary(basicSalary, benefits){
-  const grossSalary = basicSalary + benefits
-  
-
+function netSalary(basicSalary, benefits) {
+  const grossSalary = basicSalary + benefits;
 }
